3ba603e063ae6daea294a304a8c23f8a78f9dfbb
Georgi Naumov <gonaumov@gmail.com>
2020-01-16 08:41:48 +0200

    Starting work

---
+++ src/initialState.js
@@ -0,0 +1,15 @@
+const initialState = {
+    products: {
+        floorCare: {
+            description: "Cleaning your floor",
+            products: [
+                {
+                    image: "/images/Mr_Muscle_Man.jpg",
+                    description: "some description"
+                }
+            ]
+        }
+    }
+}
+
+export default initialState

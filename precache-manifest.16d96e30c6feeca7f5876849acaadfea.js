b8e787009c3b5fdc61c5737fceff44214cf04c6a
Georgi Naumov <gonaumov@gmail.com>
2020-01-17 22:02:36 +0200

    Updates

---
+++ precache-manifest.16d96e30c6feeca7f5876849acaadfea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1724c286ddadb7982eb969f3c68423a5",
+    "url": "/scjohnsonchallenge/index.html"
+  },
+  {
+    "revision": "3a1badf1e7eab96df5ee",
+    "url": "/scjohnsonchallenge/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "37aab4225dffe469eea3",
+    "url": "/scjohnsonchallenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a1badf1e7eab96df5ee",
+    "url": "/scjohnsonchallenge/static/js/2.6045e5c6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/scjohnsonchallenge/static/js/2.6045e5c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37aab4225dffe469eea3",
+    "url": "/scjohnsonchallenge/static/js/main.2d23eae8.chunk.js"
+  },
+  {
+    "revision": "cdbe14c0402e9dec8434",
+    "url": "/scjohnsonchallenge/static/js/runtime-main.6678bfce.js"
+  }
+]);

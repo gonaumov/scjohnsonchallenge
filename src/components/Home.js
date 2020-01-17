6d77efce8dc72695eeb7aa5563c1a4b4a97654dc
Georgi Naumov <gonaumov@gmail.com>
2020-01-17 22:18:39 +0200

    More fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ function Home({products, history}) {
     return (
         <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
             <header>
-                <img src="/images/toilet-duck.png" alt="Duck"/>
+                <img src="./images/toilet-duck.png" alt="Duck"/>
                 <p className="lead">
                     I am the toiled duck and I can offer you
                     many great things. Would you like to:
